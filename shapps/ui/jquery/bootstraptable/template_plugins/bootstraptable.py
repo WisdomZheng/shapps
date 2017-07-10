d4621e6a3beea842a345348648efed86845411a3
Zhe Zheng <zhengzhe2@lenovo.com>
2017-07-10 14:06:11 +0800

    Upgrate bootstrap-table from version 1.8.1 to version 1.9.0.
    Fix problem:
    1. multi-sort extension: display problem with sort arrow.
    2. Export extension could not used with jQuery version<1.8.0
    
    Add new extensions: accent-neutralise, angular, group-by, multiple-search
    
    Change-Id: I100957e183fa8db1fef72d0f37a2449625e1a387

--- shapps/ui/jquery/bootstraptable/template_plugins/bootstraptable.py
+++ shapps/ui/jquery/bootstraptable/template_plugins/bootstraptable.py
@@ -1,12 +1,20 @@
 def call(version = None, extensions = []):
     '''
-    {{use 'bootstraptable',  extensions = ["cookie"]}}
+    {{use 'bootstraptable',  version='1.9.0', extensions = ["cookie"]}}
     '''
     a = []
-    a.append('bootstraptable/bootstrap-table.js')
-    a.append('bootstraptable/bootstrap-table.css')
+    bversion = ''
+    if version:
+        bversion = 'bootstraptable-%s' % (version)
+    else:
+        bversion = 'bootstraptable'
+    a.append('%s/bootstrap-table.js' % (bversion))
+    a.append('%s/bootstrap-table.css' % (bversion))
+    css_extensions = ['group-by', 'reorder-rows']
     for ename in extensions:
-        a.append('bootstraptable/extensions/%s/bootstrap-table-%s.js'%(ename, ename))
-        if ename == "export":
-            a.append('bootstraptable/extensions/%s/tableExport.js'%(ename))
+        a.append('%s/extensions/%s/bootstrap-table-%s.js'%(bversion, ename, ename))
+        if ename in css_extensions:
+            a.append('%s/extensions/%s/bootstrap-table-%s.css'%(bversion, ename, ename))
+        if ename == 'export':
+            a.append('%s/extensions/%s/tableExport.js'%(bversion,ename))
     return {'toplinks': a}
