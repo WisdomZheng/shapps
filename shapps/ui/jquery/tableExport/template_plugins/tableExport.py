4d2ec3788a05404872f0f41dbb64b1861c07696e
Zhe Zheng <zhengzhe2@lenovo.com>
2017-08-16 14:58:36 +0800

    create local fork of TableExport(https://github.com/clarketm/TableExport)

--- shapps/ui/jquery/tableExport/template_plugins/tableExport.py
+++ shapps/ui/jquery/tableExport/template_plugins/tableExport.py
@@ -1,10 +1,16 @@
-def call(*args, **kwargs):
+def call(fork=None):
     '''
     {{use 'tableExport'}}
     '''
     a = []
-    a.append('TableExport/dist/xlsx.core.min.js')
-    a.append('TableExport/src/FileSaver.js')
-    a.append('TableExport/src/stable/js/tableexport.js')
-    a.append('TableExport/src/stable/css/tableexport.css')
+    if fork == "local":
+        a.append('local_TableExport/dist/xlsx.core.min.js')
+        a.append('local_TableExport/src/FileSaver.js')
+        a.append('local_TableExport/src/stable/js/tableexport.js')
+        a.append('local_TableExport/src/stable/css/tableexport.css')
+    else:
+        a.append('TableExport/dist/xlsx.core.min.js')
+        a.append('TableExport/src/FileSaver.js')
+        a.append('TableExport/src/stable/js/tableexport.js')
+        a.append('TableExport/src/stable/css/tableexport.css')
     return {'toplinks': a}
